5829a13fa65dbb0ab570ec64aeb1d309e1e878b7
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-07-08 09:00:54 +0530

    5. Intro to styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+// Global Style
+import GlobalStyle from "./components/GlobalStyled";
 //import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
