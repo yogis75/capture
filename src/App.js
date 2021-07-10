36c4d15cfb1367b5b6c6f75a04142205b5621c3a
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-07-10 10:11:29 +0530

    9. React Router Dom

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyled";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
