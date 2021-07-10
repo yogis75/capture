d6995230a5d179bdadf086d312136e1aed985773
Yogeshwar Suryavamshi <yogi.s75@gmail.com>
2021-07-10 09:49:02 +0530

    8. Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyled";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
